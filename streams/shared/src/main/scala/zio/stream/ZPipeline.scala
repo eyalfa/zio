2a978e38bfdc690f4f8a9241649feda9746c6552
Eyal Farago <efarago@salesforce.com>
2024-04-26 16:45:21 +0300

    strm_mapZioPar_opt: stream.mapZIOParUnordered

--- streams/shared/src/main/scala/zio/stream/ZPipeline.scala
+++ streams/shared/src/main/scala/zio/stream/ZPipeline.scala
@@ -1800,12 +1800,13 @@ object ZPipeline extends ZPipelinePlatformSpecificConstructors {
     trace: Trace
   ): ZPipeline[Env, Err, In, Out] = {
     ZPipeline.fromFunction{ (strm : ZStream[Any, Nothing, In]) =>
-      strm
+      /*strm
         .toChannel
         .concatMap(ZChannel.writeChunk(_))
         .mapOutZIOPar(n)(f)
         .mapOut(Chunk.single)
-        .toStream
+        .toStream*/
+      strm.mapZIOPar(n)(f)
     }
   }
 
