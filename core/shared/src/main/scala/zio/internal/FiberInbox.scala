a55c283620f39d8cf7375274085ba4f1729d299b
Eyal Farago <efarago@salesforce.com>
2024-06-01 15:55:56 +0300

    stream_merge_bench__inbox: dequeue optimized queue with prefetch

--- core/shared/src/main/scala/zio/internal/FiberInbox.scala
+++ core/shared/src/main/scala/zio/internal/FiberInbox.scala
@@ -3,60 +3,92 @@ package zio.internal
 import java.util.concurrent.atomic.AtomicReference
 import scala.annotation.tailrec
 
-class FiberInbox extends AtomicReference(List.empty[FiberMessage]) {
-  var dequeued, pending = List.empty[FiberMessage]
+class FiberInbox extends AtomicReference[FiberInbox.MsgQueue](FiberInbox.MsgQueue.Nil) {
+  var dequeued : FiberInbox.MsgQueue = FiberInbox.MsgQueue.Nil
   var nonEmptyHint = false
 
   def isEmpty =
     !nonEmptyHint &&
-      (this.get() eq Nil)
-
-
-  //def nonEmpty = dequeued.nonEmpty || (this.get().nonEmpty)
+      (this.get().isEmpty)
 
   def add(msg : FiberMessage): Unit = {
     nonEmptyHint = true
-    this.getAndUpdate(msg :: _)
+    this.getAndUpdate(_.enqueue(msg))
   }
 
   def addLocal(msg : FiberMessage): Unit = {
     //adds by the fiber itself, these are basically racing with external adds,
     //we can arbitrarily decide they're winning
-    pending = msg :: pending
+    dequeued = dequeued.enqueue(msg)
     nonEmptyHint = true
   }
 
   def poll() : FiberMessage = {
-    if(dequeued ne Nil) {
+    this.getOpaque
+    if(!dequeued.isEmpty) {
       val res = dequeued.head
       dequeued = dequeued.tail
       res
-    } else {
-      val reversed = if(pending ne Nil) pending else this.getAndSet(Nil)
-      pending = Nil
+    }
+    else if(this.get().isEmpty) {
       nonEmptyHint = false
-      if(reversed eq Nil) {
+      null
+    }
+    else {
+      val read = this.getAndSet(FiberInbox.MsgQueue.Nil)
+      if(read.isEmpty) {
+        nonEmptyHint = false
         null
       } else {
-        @tailrec def go(rest: ::[FiberMessage]): FiberMessage = {
-          val t = rest.tail
-          if (t eq Nil)
-            rest.head
-          else {
-            dequeued = rest.head :: dequeued
-            nonEmptyHint = true
-            go(t.asInstanceOf[::[FiberMessage]])
-          }
-        }
-
-        go(reversed.asInstanceOf[::[FiberMessage]])
+        dequeued = read.tail
+        nonEmptyHint = !dequeued.isEmpty
+        read.head
       }
     }
   }
-
-
 }
 
 object FiberInbox {
-  class Node(val msg : FiberMessage, var next : Node)
+  sealed trait MsgQueue {
+    val isEmpty : Boolean = false
+    def head : FiberMessage
+    def tail : MsgQueue
+
+    def enqueue(msg : FiberMessage) : MsgQueue
+  }
+
+  object MsgQueue {
+    //read optimized queue impl
+    case object Nil extends MsgQueue {
+
+      override val isEmpty: Boolean = true
+
+      override def head: FiberMessage = ???
+
+      override def tail: MsgQueue = this
+
+      def enqueue(msg : FiberMessage) =
+        Single(msg)
+    }
+
+    case class Single(head: FiberMessage) extends MsgQueue {
+      override def tail: MsgQueue = Nil
+
+      def enqueue(msg : FiberMessage) =
+        Two(head, msg)
+    }
+
+    case class Two(head : FiberMessage, next : FiberMessage) extends MsgQueue {
+      override lazy val tail: MsgQueue = Single(next)
+      def enqueue(msg : FiberMessage) =
+        Cons(head,
+          Two(next, msg)
+        )
+    }
+
+    case class Cons(head : FiberMessage, tail : MsgQueue) extends MsgQueue {
+      def enqueue(msg : FiberMessage) =
+        copy(tail = tail.enqueue(msg))
+    }
+  }
 }
